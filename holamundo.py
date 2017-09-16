2ebd8b03970f7fa11e4b7fa6effbb5521df3a32b
ama0114 <ama0114@alu.ubu.es>
2017-09-16 16:15:45 +0200

    Comprobamos la correcta importación y ejecución de las librerias que vamos a usar.

---
+++ holamundo.py
@@ -0,0 +1,10 @@
+"""Codigo que comprueba la correcta importacion y funcionamiento
+    de OpenCV, matplotlib y numpy.
+"""
+import cv2
+import matplotlib
+import numpy
+print "Hola Mundo OpenCV"
+print cv2.__version__
+print matplotlib.__version__
+print numpy.__version__
