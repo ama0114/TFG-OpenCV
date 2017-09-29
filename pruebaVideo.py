275706dc992ddd4c1bac53219a287c6f24411ae7
ama0114 <ama0114@alu.ubu.es>
2017-09-29 17:40:35 +0200

    Últimos cambios a la prueba de reproducción de vídeo.

--- pruebaVideo.py
+++ pruebaVideo.py
@@ -1,5 +1,7 @@
 """Reproducción de un archivo de video 
     con OpenCV.
+    Código obtenido en:
+        http://docs.opencv.org/3.0-beta/doc/py_tutorials/py_gui/py_video_display/py_video_display.html
 """
 import cv2
 
@@ -8,10 +10,14 @@ cap = cv2.VideoCapture('recIzq.avi')
 
 #Mientras el video esta abierto
 while cap.isOpened():
+
+    #Leemos por fotogramas el video
     ret, frame = cap.read()
 
+    #Interpretamos el video en escala de grises, suficiente para poder detectar la linea
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+    #Visualizamos el video
     cv2.imshow('frame', gray)
 
     #Lo podemos cerrar presionando la tecla q
