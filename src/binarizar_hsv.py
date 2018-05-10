610c286ae45385d09540953d5c57815db566e856
Antonio <ama0114@alu.ubu.es>
2018-05-10 12:30:58 +0200

    Binarizar HSV
    Prueba de binarizacion en hsv, por rango de color. En este caso usamos una linea de color amarillo. Independientemente del color de fondo, el programa es capaz de detectar la linea.

---
+++ src/binarizar_hsv.py
@@ -0,0 +1,33 @@
+# coding=utf-8
+from webcam_stream import webcam_stream
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+import toolbox
+
+def main():
+    stream = webcam_stream('http://192.168.1.10:8080/shot.jpg')
+    
+    while True:
+        #0 - B/N, 1 - Color RGB
+        vid, fps = stream.get_video_stream(1)
+
+        hsv =  hsv = cv2.cvtColor(vid, cv2.COLOR_BGR2HSV)
+
+        # define range of yellow color in HSV
+        lower_y = np.array([20,10,60])
+        upper_y = np.array([40,255,255])
+
+        #Binarizo
+        img_binarizada = cv2.inRange(hsv, lower_y, upper_y)
+
+        #Muestro la imagen
+        cv2.imshow('Bin', img_binarizada)
+        cv2.imshow('Original', vid)
+
+        # salimos pulsando s
+        if cv2.waitKey(1) & 0xFF == ord('s'):
+            break
+
+if  __name__ =='__main__':
+    main()
