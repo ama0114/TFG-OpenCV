6144d638971a7c8697ad9643e1342b0f048dace1
Antonio <ama0114@alu.ubu.es>
2018-06-15 00:58:09 +0200

    Mejoras dirección
    Redirigir hasta el centro de la imagen y no hasta el borde de la zona segura

--- src/direccion.py
+++ src/direccion.py
@@ -4,27 +4,28 @@ import numpy as np
 
 class direccion:
 
-    f_derecha = None
-    f_izquierda = None
-    rango_seguro_min = 0
-    rango_seguro_max = 0
-
     def __init__(self, error, tam_horiz_frame):
         self.rango_seguro_min = (tam_horiz_frame/2)-(tam_horiz_frame*error/2)
-        x = [0,(self.rango_seguro_min/2),self.rango_seguro_min]
-        y = [90,45,0]
-        slope_1, intercept_1, _, _, _ = stats.linregress(x, y)
-        self.f_derecha = lambda i: float(intercept_1) + float(slope_1)*i
-
+        self.rango_seguro_min_bkp = (tam_horiz_frame/2)-(tam_horiz_frame*error/2)
         self.rango_seguro_max = (tam_horiz_frame/2)+(tam_horiz_frame*error/2)
+        self.rango_seguro_max_bkp = (tam_horiz_frame/2)+(tam_horiz_frame*error/2)
+        self.f_derecha = self.calcular_fcn_drch()
+        self.f_izquierda = self.calcular_fcn_izq(tam_horiz_frame)
+
+    def calcular_fcn_izq(self, tam_horiz_frame):
         x = [self.rango_seguro_max,
             self.rango_seguro_max+((tam_horiz_frame-self.rango_seguro_max)/2),
             tam_horiz_frame]
         y = [0,45,90]
         slope_2, intercept_2, _, _, _ = stats.linregress(x, y)
-        self.f_izquierda = lambda j: float(intercept_2) + float(slope_2)*j
-        
+        return lambda j: float(intercept_2) + float(slope_2)*j
 
+    def calcular_fcn_drch(self):
+        x = [0,(self.rango_seguro_min/2),self.rango_seguro_min]
+        y = [90,45,0]
+        slope_1, intercept_1, _, _, _ = stats.linregress(x, y)
+        return lambda i: float(intercept_1) + float(slope_1)*i
+        
     def obtener_direccion(self, frame):
         angulo = 0
         texto = "R"
@@ -33,6 +34,12 @@ class direccion:
 
             #Girar derecha
             if pos < self.rango_seguro_min:
+                if pos in range(int(len(frame[0])/2)-2, int(len(frame[0])/2)+2):
+                    self.rango_seguro_min = self.rango_seguro_min_bkp
+                else:
+                    self.rango_seguro_min = len(frame[0])/2
+
+                self.f_derecha = self.calcular_fcn_drch()
                 angulo = self.f_derecha(pos)
                 if angulo > 45:
                     texto = "I"
@@ -41,6 +48,12 @@ class direccion:
                 
             #Girar izquierda
             if pos > self.rango_seguro_max:
+                if pos in range(int(len(frame[0])/2)-2, int(len(frame[0])/2)+2):
+                    self.rango_seguro_max = self.rango_seguro_max_bkp
+                else:
+                    self.rango_seguro_max = len(frame[0])/2
+
+                self.f_izquierda = self.calcular_fcn_izq(len(frame[0]))
                 angulo = self.f_izquierda(pos)
                 if angulo > 45:
                     texto = "D"
